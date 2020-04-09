de7223fef4343ce67aef338624fcb9d5e2b73730
Georgich88 <georgeisaev@gmail.com>
2020-04-10 00:16:22 +0700

    Task 17 - Hibernate

--- src/main/java/com/foxminded/rodin/timetable/repo/SlotRepository.java
+++ src/main/java/com/foxminded/rodin/timetable/repo/SlotRepository.java
@@ -22,4 +22,7 @@ public interface SlotRepository extends CrudRepository<Slot, Long> {
     @Query(value = "SELECT t FROM Teacher t LEFT JOIN Slot s ON s.instructor = t  WHERE s.id IS NULL OR NOT (s.id != ?1 AND s.startTime >= ?2 AND s.endTime < ?3)")
     public List<Teacher> findAvailableTeachers(Long id, LocalDateTime startDate, LocalDateTime endDate);
 
+    @Query(value = "SELECT s FROM Slot s INNER JOIN Group g ON g = s.group INNER JOIN g.students st WHERE st.id = ?1")
+    public List<Slot> findSlotsByStudentId(Long id);
+
 }
